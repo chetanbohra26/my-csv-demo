7474b6c9993775147b877d09bef7e7103d956a5d
Chetan Bohra <chetanbohra26@gmail.com>
2021-01-15 00:03:06 +0530

    added more endpoints

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const path = require("path");
 
+app.get("/", (req, res) => {
+	res.send({
+		api: [{ "/emp": "Employee data" }, { "/project": "Project data" }],
+	});
+});
+
 app.get("/emp", (req, res) => {
 	res.attachment("Employee.csv").sendFile(
 		path.join(__dirname, "Employee.csv")
@@ -11,6 +17,10 @@ app.get("/emp", (req, res) => {
 
 app.get("/project", (req, res) => {
 	res.attachment("Project.csv").sendFile(path.join(__dirname, "Project.csv"));
+});
+
+app.get("*", (req, res) => {
+	res.redirect("/");
 });
 
 const port = process.env.PORT || 7500;
